152eb7e0984be0d572433167f839bc34db3fafb5
ZakharFilip <157304145+ZakharFilip@users.noreply.github.com>
2025-06-05 23:08:43 +0300

    First YEEES

---
+++ app/src/main/java/com/example/parlamentssandroid/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.parlamentssandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register) // Привязываем XML-макет
+
+        // Здесь будет обработка кнопок
+    }
+}
